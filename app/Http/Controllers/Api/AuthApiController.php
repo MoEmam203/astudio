5f15e9fcbe087e247241ed9a287a8bce33cd829b
Mustafa Shoukri <m.shoukri@geeksjo.com>
2025-03-08 15:06:21 +0200

    Feat: make edit to auth api controller

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -6,56 +6,27 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\LoginApiRequest;
 use App\Http\Requests\Api\Auth\RegisterApiRequest;
 use App\Models\User;
+use App\Services\Api\AuthApiService;
 use Illuminate\Http\JsonResponse;
 
 class AuthApiController extends Controller
 {
+    public function __construct(public readonly AuthApiService $authApiService)
+    {
+
+    }
     public function register(RegisterApiRequest $request): JsonResponse
     {
-        $user =  User::create($request->validated());
-        $token = $user->createToken('auth_token')->accessToken;
-
-        return response()->json(data: [
-            'status' => 1,
-            'message' => 'User created successfully',
-            'data' => [
-                'user' => $user,
-                'token' => $token
-            ]
-        ]);
+        return $this->authApiService->register(validatedData: $request->validated());
     }
 
     public function login(LoginApiRequest $request): JsonResponse 
     {
-        if (!auth()->attempt($request->only('email', 'password'))) {
-            return response()->json(data: [
-                'status' => 0,
-                'message' => 'Invalid credentials',
-                'data' => null
-            ], status: 401);
-        }
-
-        $user = auth()->user();
-        $token = $user->createToken('auth_token')->accessToken;
-
-        return response()->json(data: [
-            'status' => 1,
-            'message' => 'User logged in successfully',
-            'data' => [
-                'user' => $user,
-                'token' => $token
-            ]
-        ]);
+        return $this->authApiService->login(credentials: $request->only('email', 'password'));
     }
 
     public function logout(): JsonResponse
     {
-        auth()->user()->token()->revoke();
-
-        return response()->json(data: [
-            'status' => 1,
-            'message' => 'User logged out successfully',
-            'data' => null
-        ]);
+        return $this->authApiService->logout();
     }
 }
